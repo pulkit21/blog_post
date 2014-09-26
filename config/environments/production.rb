9319c5f081fa4845f44c503077e368c3e48801b3
Pulkit Agarwal <pulkit.ag02@gmail.com>
2014-09-26 18:42:38 +0530

    fix smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host => 'blogposts.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'blogposts.herokuapp.com' }
 
   #config.action_mailer.default_url_options = { host => '*.emangu.com' }
   config.action_mailer.delivery_method = :smtp
